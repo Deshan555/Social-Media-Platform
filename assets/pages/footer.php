62d6ac1be6591e1ce10272e4d2bedd7139e2d364
D.J Amarasinghe <74134064+Deshan555@users.noreply.github.com>
2022-11-03 10:14:00 +0530

    remake folder structure and remove previous files and unwanted folders from the repo

--- assets/pages/footer.php
+++
@@ -1,5 +0,0 @@
-<script src="assets/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-</body>
-
-</html>
--- assets/php/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-// mysqli database connection
-const DB_NAME = 'pictogram';
-const DB_HOST = 'localhost';
-const DB_USER = 'root';
-const DB_PASS = 'root';
