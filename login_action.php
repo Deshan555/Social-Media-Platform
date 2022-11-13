6ad4da67c4f06e56c62d7969d533567018d98af0
D.J Amarasinghe <74134064+Deshan555@users.noreply.github.com>
2022-11-13 13:54:40 +0530

    finish backend of login #7

---
+++ login_action.php
@@ -0,0 +1,98 @@
+<?php
+
+include('config.php');
+
+if(isset($_POST['button']))
+{
+    $fisrt_parm = $_POST['email'];
+
+    $password = md5($_POST['password']);
+
+    $full_name = "fullname";
+
+    $user_name= "username";
+
+    $user_type = "1";
+
+    $email_address= "example@gmail.com";
+
+    $facebook = "www.facebook.com";
+
+    $whatsapp = "www.webwhatsapp.com";
+
+    $bio = "tell use more about you";
+
+    $fallowers = 0;
+
+    $fallowing = 0;
+
+    $post_count = 0;
+
+    $image = "assets/images/default.png";
+
+    $user_id = 000000;
+
+    $sql_query = "SELECT User_ID, FULL_NAME, USER_NAME, USER_TYPE, EMAIL, IMAGE, FACEBOOK, WHATSAPP, BIO, FALLOWERS, FALLOWING, POSTS  
+    FROM USERS WHERE USER_NAME = '$fisrt_parm' AND PASSWORD_S = '$password';";
+
+    //echo $sql_query;
+
+    $stmt = $conn->prepare($sql_query);
+
+    $stmt->execute();
+
+    $stmt->store_result();
+
+    if($stmt->num_rows() > 0)
+    {
+        //echo 'fail';
+
+        $stmt->bind_result($user_id, $full_name, $user_name, $user_type, $email_address, $image, $facebook, $whatsapp, $bio, $fallowers, $fallowing, $post_count);
+        
+        $stmt->fetch();
+
+        $_SESSION['id'] = $user_id;
+
+        $_SESSION['username'] = $user_name;
+
+        $_SESSION['fullname'] = $full_name;
+
+        $_SESSION['email'] = $email_address;
+
+        $_SESSION['usertype'] = $user_type;
+
+        $_SESSION['facebook'] = $facebook;
+
+        $_SESSION['whatsapp'] = $whatsapp;
+
+        $_SESSION['bio'] = $bio;
+
+        $_SESSION['fallowers'] = $fallowers;
+
+        $_SESSION['fallowing'] = $fallowing;
+
+        $_SESSION['postcount'] = $post_count;
+
+        $_SESSION['img_path'] = $image;
+
+        header("location: index.html");
+    }
+    else{
+
+        //echo 'fail';
+
+        header('location: login.php?error_message=Email/Password Incorrect');
+
+        exit;
+    }
+}
+else{
+
+    //echo 'fail';
+
+    header('location: login.php?error_message=Some Error Happend');
+
+    exit;
+}
+
+?>
