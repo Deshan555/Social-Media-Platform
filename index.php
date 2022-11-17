564bb55c822751b423ca21d6260deed0ae2af70f
D.J Amarasinghe <74134064+Deshan555@users.noreply.github.com>
2022-11-17 17:32:34 +0530

    update index.php file

--- index.php
+++ index.php
@@ -1,407 +1,67 @@
-
-<?php 
-
-session_start();
-
-if(!isset($_SESSION['id']))
-{
-  header('location: login.php');
-
-  exit;
-}
-
-?>
-
 <!DOCTYPE html>
 
-<html lang="en">
+<html>
 
 <head>
+    <title>EventsWave</title>
+    
     <meta charset="UTF-8">
-
-    <title>Title</title>
-
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.rtl.min.css" integrity="sha384-+qdLaIRZfNu4cVPK/PxJJEy0B0f3Ugv8i482AKY7gwXwhaCroABd086ybrVKTa0q" crossorigin="anonymous">
-
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous"/>
-
-    <title>Document</title>
-
-    <link rel="stylesheet" href="assets/css/style.css">
-
-    <link rel="stylesheet" href="assets/css/section.css">
-
-    <link rel="stylesheet" href="assets/css/posting.css">
-
-    <link rel="stylesheet" href="assets/css/right_col.css">
-
-    <link rel="stylesheet" href="assets/css/responsive.css">
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+    
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.2/css/all.css" />
+    
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" />
+    
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-login-form.min.css" />
+    
+    <style>
+    
+    body,h1
+    {
+      font-family: "Raleway", sans-serif
+    }
+    
+    body, html 
+    {
+      height: 100%
+    }
+    
+    .bgimg 
+    {
+      background-image: url('assets/images/login_request/cover.png');
+      
+      min-height: 100%;
+      
+      background-position: center;
+      
+      background-size: cover;
+    }
+    </style>
 </head>
+
 
 <body>
 
-<!-- Nav Bar Design -->
+<div class="bgimg w3-display-container w3-animate-opacity w3-text-white">
 
-  <nav class="navbar">
+  <div class="w3-display-middle">
 
-      <div class="nav-wrapper">
+    <h1 class="w3-jumbo w3-animate-top"><img class="mb-4" src="assets/images/login_request/small_logo.png" alt="" height="55"></h1>
 
-          <img src="assets/images/black_logo.png" class="brand-img">
+    <hr class="w3-border-grey" style="margin:auto;width:40%">
 
-          <form>
-              <input type="text" class="search-box" placeholder="search">
-          </form>
+    <p class="w3-large w3-center">Events with Elegance</p>
 
-          <div class="nav-items">
+    <p class="w3-large w3-center"><a href="selection_page.php"><button type="button" class="btn btn-outline-light" data-mdb-ripple-color="dark">Get Started</button></a></p>
 
-              <i class="icon fas fa-home fa-lg"></i>
-
-              <i class="icon fas fa-plus-square fa-lg"></i>
-
-              <i class="icon fas fa-calendar-alt fa-lg"></i>
-
-              <i class="icon fas fa-heart fa-lg"></i>
-
-              <div class="icon user-profile">
-
-                  <i class="fas fa-user-circle fa-lg"></i>
-
-              </div>
-
-          </div>
-
-      </div>
-
-  </nav>
-
-
-<!-- New Section -->
-
-
-<section class="main">
-
-    <div class="wrapper">
-
-        <!-- Design for left column -->
-
-        <div class="left-col">
-
-            <!-- Wrapper For Status -->
-
-            <div class="status-wrapper">
-
-                <!-- sample data -->
-
-                <div class="status-card">
-
-                    <div class="profile-pic"><img src="assets/images/default.png"> </div>
-
-                    <p class="username">User Name</p>
-
-                </div>
-
-                <div class="status-card">
-
-                    <div class="profile-pic"><img src="assets/images/default.png"> </div>
-
-                    <p class="username">User Name</p>
-
-                </div>
-
-                <div class="status-card">
-
-                    <div class="profile-pic"><img src="assets/images/default.png"> </div>
-
-                    <p class="username">User Name</p>
-
-                </div>
-
-                <div class="status-card">
-
-                    <div class="profile-pic"><img src="assets/images/default.png"> </div>
-
-                    <p class="username">User Name</p>
-
-                </div>
-
-                <div class="status-card">
-
-                    <div class="profile-pic"><img src="assets/images/default.png"> </div>
-
-                    <p class="username">User Name</p>
-
-                </div>
-
-                <div class="status-card">
-
-                    <div class="profile-pic"><img src="assets/images/default.png"> </div>
-
-                    <p class="username">User Name</p>
-
-                </div>
-
-                <div class="status-card">
-
-                    <div class="profile-pic"><img src="assets/images/default.png"> </div>
-
-                    <p class="username">User Name</p>
-
-                </div>
-
-                <div class="status-card">
-
-                    <div class="profile-pic"><img src="assets/images/default.png"> </div>
-
-                    <p class="username">User Name</p>
-
-                </div>
-
-                <div class="status-card">
-
-                    <div class="profile-pic"><img src="assets/images/default.png"> </div>
-
-                    <p class="username">User Name</p>
-
-                </div>
-
-            </div>
-
-            <!-- Wrapper for posting -->
-
-            <div class="post">
-
-                <div class="info">
-
-                    <div class="user">
-
-                        <div class="profile-pic"><img src="assets/images/default.png"></div>
-
-                        <p class="username">SLTC Leo Club</p>
-
-                    </div>
-
-                    <i class="fas fa-ellipsis-v options"></i>
-
-                </div>
-
-                <img src="assets/images/posts/post.jpg" class="post-img">
-
-                <div class="post-content">
-
-                    <div class="reactions-wrapper">
-
-                        <i class="icon fas fa-thumbs-up"></i>
-
-                        <i class="icon fas fa-comment"></i>
-
-                        <i class="icon fas fa-calendar-alt"></i>
-
-                    </div>
-
-                    <p class="reactions">1,789 Reactions</p>
-
-                    <p class="description">
-                        <span>Username Says : <br></span>
-                        I got my peaches out in Georgia (oh, yeah, shit)
-                        I get my weed from California (that's that shit)
-                        I took my chick up to the North, yeah (badass bitch)
-                        I get my light right from the source, yeah (yeah, that's it)
-                    </p>
-
-                    <p class="post-time">2022/11/5</p>
-
-                </div>
-
-                <div class="comments-section">
-
-                    <img src="assets/images/default.png" class="icon">
-
-                    <input type="text" class="comment-box" placeholder="Your Opinion">
-
-                    <button class="comment-button">WRITE</button>
-
-                </div>
-
-            </div>
-
-            <div class="post">
-
-                <div class="info">
-
-                    <div class="user">
-
-                        <div class="profile-pic"><img src="assets/images/default.png"></div>
-
-                        <p class="username">SLTC Leo Club</p>
-
-                    </div>
-
-                    <i class="fas fa-ellipsis-v options"></i>
-
-                </div>
-
-                <img src="assets/images/posts/post.jpg" class="post-img">
-
-                <div class="post-content">
-
-                    <div class="reactions-wrapper">
-
-                        <i class="icon fas fa-thumbs-up"></i>
-
-                        <i class="icon fas fa-comment"></i>
-
-                        <i class="icon fas fa-calendar-alt"></i>
-
-                    </div>
-
-                    <p class="reactions">1,789 Reactions</p>
-
-                    <p class="description">
-                        <span>Username Says : <br></span>
-                        I got my peaches out in Georgia (oh, yeah, shit)
-                        I get my weed from California (that's that shit)
-                        I took my chick up to the North, yeah (badass bitch)
-                        I get my light right from the source, yeah (yeah, that's it)
-                    </p>
-
-                    <p class="post-time">2022/11/5</p>
-
-                </div>
-
-                <div class="comments-section">
-
-                    <img src="assets/images/default.png" class="icon">
-
-                    <input type="text" class="comment-box" placeholder="Your Opinion">
-
-                    <button class="comment-button">WRITE</button>
-
-                </div>
-
-            </div>
-
-        </div>
-
-        <!-- Design for right column -->
-
-        <div class="right-col">
-
-            <!-- structure for profile card section-->
-
-            <div class="profile_card">
-
-                <div class="profile-pic">
-
-                    <img src="assets/images/default.png">
-
-                </div>
-
-                <div>
-
-                    <p class="username">EventsWave Official</p>
-
-                    <p class="sub-text">Events with Elegance</p>
-
-                </div>
-
-                <form method="GET" action="logout.php">
-                    <button class="logout-btn">LogOut</button>
-                </form>
-
-            </div>
-
-            <!-- structure for suggestions -->
-
-            <p class="suggesting">Recommendation For You</p>
-
-            <div class="suggestion_card">
-
-                <div class="suggestions-pic">
-
-                    <img src="assets/images/profile_2.jpg">
-
-                </div>
-
-                <div>
-
-                    <p class="username">Naruto_Uz890</p>
-
-                    <p class="sub-text">Naruto Uzumaki</p>
-
-                </div>
-
-                <button class="fallow-btn">Fallow</button>
-
-            </div>
-
-            <div class="suggestion_card">
-
-                <div class="suggestions-pic">
-
-                    <img src="assets/images/profile_2.jpg">
-
-                </div>
-
-                <div>
-
-                    <p class="username">Naruto_Uz890</p>
-
-                    <p class="sub-text">Naruto Uzumaki</p>
-
-                </div>
-
-                <button class="fallow-btn">Fallow</button>
-
-            </div>
-
-            <div class="suggestion_card">
-
-                <div class="suggestions-pic">
-
-                    <img src="assets/images/profile_2.jpg">
-
-                </div>
-
-                <div>
-
-                    <p class="username">Naruto_Uz890</p>
-
-                    <p class="sub-text">Naruto Uzumaki</p>
-
-                </div>
-
-                <button class="fallow-btn">Fallow</button>
-
-            </div>
-
-            <div class="suggestion_card">
-
-                <div class="suggestions-pic">
-
-                    <img src="assets/images/profile_2.jpg">
-
-                </div>
-
-                <div>
-
-                    <p class="username">Naruto_Uz890</p>
-
-                    <p class="sub-text">Naruto Uzumaki</p>
-
-                </div>
-
-                <button class="fallow-btn">Fallow</button>
-
-            </div>
-
-        </div>
-
-    </div>
-
-</section>
+  </div>
+  
+</div>
 
 </body>
 
