2a867572f9ee232f455ab5f016b7e8fee0d8de9c
D.J Amarasinghe <74134064+Deshan555@users.noreply.github.com>
2022-12-01 20:50:42 +0530

    Create get_suggestions.php

---
+++ get_suggestions.php
@@ -0,0 +1,36 @@
+<?php 
+
+include("config.php");
+
+$user_id = $_SESSION['id'];
+
+$sql_query = "SELECT Other_user_id FROM fallowing WHERE User_Id = $user_id;";
+
+$stmt =  $conn->prepare($sql_query);
+
+$stmt->execute();
+
+$ids = array();
+
+$result = $stmt->get_result();
+
+while($row = $result->fetch_array(MYSQLI_NUM))
+{
+    foreach($row as $rows)
+    {
+        $ids[] = $rows;
+    }
+}
+
+
+$fallowing_id = join("','",$ids);
+
+$sql_query_two = "SELECT * FROM Users WHERE User_Id NOT IN($fallowing_id) ORDER BY RAND() LIMIT 4;";
+
+$stmt = $conn->prepare($sql_query_two);
+
+$stmt->execute();
+
+$suggestions = $stmt->get_result();
+
+?>
