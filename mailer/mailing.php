585794bac9ddbb0f8d70d999a1f3ab126facc1f8
D.J Amarasinghe <74134064+Deshan555@users.noreply.github.com>
2022-12-04 19:03:40 +0530

    update php mailer package

---
+++ mailer/mailing.php
@@ -0,0 +1,82 @@
+<?php
+
+session_start();
+
+if(!isset($_SESSION['id']))
+{
+  header('location: login.php');
+
+  exit;
+}
+
+$sending_person = $_SESSION['email'];
+
+echo $sending_person;
+
+use PHPMailer\PHPMailer\PHPMailer;
+
+use PHPMailer\PHPMailer\SMTP;
+
+use PHPMailer\PHPMailer\Exception;
+
+require_once "vendor/autoload.php";
+
+$mail = new PHPMailer(true);
+
+//Enable SMTP debugging.
+
+$mail->SMTPDebug = 3;                               
+
+//Set PHPMailer to use SMTP.
+
+$mail->isSMTP();            
+
+//Set SMTP host name                          
+
+$mail->Host = "smtp.gmail.com";
+
+//Set this to true if SMTP host requires authentication to send email
+
+$mail->SMTPAuth = true;                          
+
+//Provide username and password     
+
+$mail->Username = "deshanja@sltc.ac.lk";                 
+
+$mail->Password = "cweorfeorufthkbf";                           
+
+//If SMTP requires TLS encryption then set it
+
+$mail->SMTPSecure = "tls";                           
+
+//Set TCP port to connect to
+
+$mail->Port = 587;                                   
+
+$mail->From = "dj.amarasinghe.dev@gmail.com";
+
+$mail->FromName = "Full Name";
+
+$mail->addAddress("maxwon555@gmail.com", "Recepient Name");
+
+$mail->isHTML(true);
+
+$mail->Subject = "Subject Text";
+
+$mail->Body = "<i>Mail body in HTML</i>";
+
+$mail->AltBody = "This is the plain text version of the email content";
+
+try 
+{
+    $mail->send();
+
+    echo "Message has been sent successfully";
+} 
+catch (Exception $e) 
+{
+    echo "Mailer Error: " . $mail->ErrorInfo;
+}
+
+
+?>
