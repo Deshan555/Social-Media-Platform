a85b5686e04d3b896a99a9edebf5357454b54cb0
D.J Amarasinghe <74134064+Deshan555@users.noreply.github.com>
2022-12-06 21:09:29 +0530

    add like.php for react posts

---
+++ like.php
@@ -0,0 +1,44 @@
+<?php
+
+session_start();
+
+include("config.php");
+
+if (isset($_POST['reaction'])) {
+
+    $user_id = $_SESSION['id'];
+
+    $post_id = $_POST['post_id'];
+
+    $SQL = "INSERT INTO likes(Post_ID, User_ID)VALUES($post_id, $user_id);";
+
+    $stmt = $conn->prepare($SQL);
+
+    $stmt->execute();
+
+    $conn->close();
+
+    update_likes($post_id);
+
+    $_SESSION['fallowing'] = $_SESSION['fallowing'] + 1;
+
+    header("location: home.php");
+
+} else {
+
+    header("location: home.php");
+}
+
+
+function update_likes($post_id)
+{
+    include("config.php");
+
+    $sql = "UPDATE posts SET Likes = Likes+1 WHERE Post_ID = $post_id;";
+
+    $stmt = $conn->prepare($sql);
+
+    $stmt->execute();
+}
+
+?>
