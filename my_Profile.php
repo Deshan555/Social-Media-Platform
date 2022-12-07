5d8ae38ee5e68bdc681ba4769788384b04a436c5
D.J Amarasinghe <74134064+Deshan555@users.noreply.github.com>
2022-12-07 20:10:22 +0530

    add pagination bar to my_Profile.php

--- my_Profile.php
+++ my_Profile.php
@@ -248,6 +248,24 @@ if(!isset($_SESSION['id']))
 
     </div>
 
+<nav aria-label="Page navigation example" style="display:flex; justify-content: center">
+    <ul class="pagination">
+        <li class="page-item <?php if($page_no<=1){echo 'disabled';}?>">
+            <a class="page-link" href="<?php if($page_no<=1){echo'#';}else{ echo '?page_no='. ($page_no-1); }?>">Previous</a>
+        </li>
+        <li class="page-item"><a class="page-link" href="?page_no=1">1</a></li>
+        <li class="page-item"><a class="page-link" href="?page_no=2">2</a></li>
+        <li class="page-item"><a class="page-link" href="?page_no=3">3</a></li>
+        <?php if($page_no >= 3){?>
+            <li class="page-item"><a class="page-link" href="#">...</a></li>
+            <li class="page-item"><a class="page-link" href="<?php echo "?page_no=". $page_no;?>"></a></li>
+        <?php } ?>
+        <li class="page-item <?php if($page_no>= $total_no_of_pages){echo 'disabled';}?>">
+            <a class="page-link" href="<?php if($page_no>=$total_no_of_pages){echo "#";}else{ echo "?page_no=".($page_no+1);}?>">Next</a>
+        </li>
+    </ul>
+</nav>
+
 </body>
 
 <script src="notifast/notifast.min.js"></script>
