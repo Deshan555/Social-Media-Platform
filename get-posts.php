3ca4632280d0319cdb7763ea0c5ed25f5c60d717
D.J Amarasinghe <74134064+Deshan555@users.noreply.github.com>
2022-12-07 20:10:55 +0530

    limit post getting count and update file

--- get-posts.php
+++ get-posts.php
@@ -1,26 +1,42 @@
 <?php
 
-//session_start();
-
 include('config.php');
 
-$ID = $_SESSION['id'];
-
-$sql = "SELECT * FROM Posts WHERE User_ID = $ID ;";
-
-//echo $sql;
-
-$stmt = $conn->prepare($sql);
-
-if($stmt->execute())
+if( isset($_GET['page_no']) && $_GET['page_no'] != "")
 {
-    $posts = $stmt->get_result();
+    $page_no = $_GET['page_no'];
 }
 else
 {
-    // don't need to anything
-
-    $posts = [];
+    $page_no = 1;
 }
+
+$user_id = $_SESSION['id'];
+
+$stmt = $conn->prepare("SELECT COUNT(*) as total_posts FROM posts WHERE User_ID = ?");
+
+$stmt->bind_param("i",$user_id);
+
+$stmt->execute();
+
+$stmt->bind_result($total_posts);
+
+$stmt->store_result();
+
+$stmt->fetch();
+
+$total_posts_per_page = 20;
+
+$offset = ($page_no-1) * $total_posts_per_page;
+
+$total_no_of_pages = ceil($total_posts/$total_posts_per_page);
+
+$stmt = $conn->prepare("SELECT * FROM posts WHERE User_ID = ?  ORDER BY Post_ID DESC LIMIT $offset,$total_posts_per_page");
+
+$stmt->bind_param("i",$user_id);
+
+$stmt->execute();
+
+$posts = $stmt->get_result();
 
 ?>
