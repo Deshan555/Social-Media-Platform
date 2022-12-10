c302d66104f7c07e3e754265c522ea7ed9e5ec28
D.J Amarasinghe <74134064+Deshan555@users.noreply.github.com>
2022-12-10 18:17:56 +0530

    additional file for video wall

---
+++ check_like_statusVid.php
@@ -0,0 +1,24 @@
+<?php
+
+include('config.php');
+
+$user_id = $_SESSION['id'];
+
+$post_id = $post['Video_ID'];
+
+$sql = "SELECT * FROM likes_vid WHERE User_ID = $user_id AND Video_ID = $post_id;";
+
+$stmt = $conn->prepare($sql);
+
+$stmt->execute();
+
+$stmt->store_result();
+
+if ($stmt->num_rows() > 0)
+{
+    $reaction_status = true;
+}else
+{
+    $reaction_status = false;
+}
+
