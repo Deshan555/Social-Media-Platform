1923ef5be1013158c98e99c34dfbc511ac26e1d7
D.J Amarasinghe <74134064+Deshan555@users.noreply.github.com>
2022-12-11 09:56:50 +0530

    add two functions for search-events by name and search-videos by given name

--- Results_Provider.php
+++ Results_Provider.php
@@ -1,5 +1,7 @@
 <?php
 
+session_start();
+
 function find_Users($search_input)
 {
     include "config.php";
@@ -14,6 +16,71 @@ function find_Users($search_input)
 
     return $users;
 }
+
+function find_Events($search_input)
+{
+    include "config.php";
+
+    $SQL = "SELECT * FROM events WHERE Caption LIKE '%$search_input%' OR HashTags LIKE '%$search_input%';";
+
+    $stmt = $conn->prepare($SQL);
+
+    $stmt->execute();
+
+    $event = $stmt->get_result();
+
+    return $event;
+}
+
+function find_Shorts($search_input)
+{
+    include "config.php";
+
+    $SQL = "SELECT * FROM videos WHERE CAPTION LIKE '%$search_input%' OR HashTags LIKE '%$search_input%';";
+
+    $stmt = $conn->prepare($SQL);
+
+    $stmt->execute();
+
+    $shorts = $stmt->get_result();
+
+    return $shorts;
+}
+
+function get_My_Followers()
+{
+    include "config.php";
+
+    $my_id = $_SESSION['id'];
+
+    $SQL = "SELECT * FROM fallowing WHERE Other_user_id = $my_id;";
+
+    $stmt = $conn->prepare($SQL);
+
+    $stmt->execute();
+
+    $users = $stmt->get_result();
+
+    return $users;
+}
+
+function get_My_Followings()
+{
+    include "config.php";
+
+    $my_id = $_SESSION['id'];
+
+    $SQL = "SELECT * FROM fallowing WHERE User_Id = $my_id;";
+
+    $stmt = $conn->prepare($SQL);
+
+    $stmt->execute();
+
+    $users = $stmt->get_result();
+
+    return $users;
+}
+
 
 
 
