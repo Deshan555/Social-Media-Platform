2e39aae5bce43be6a0488ff792966ade4bc7cce7
D.J Amarasinghe <74134064+Deshan555@users.noreply.github.com>
2022-12-26 15:15:57 +0530

    remove isset function and reprogramme source code set suitable for ajax post requests

--- unlike_event.php
+++ unlike_event.php
@@ -4,37 +4,29 @@ session_start();
 
 include("config.php");
 
-if (isset($_POST['reaction'])) {
+$post_id = isset($_POST['post_id']) ? $_POST['post_id'] : '';
 
-    $user_id = $_SESSION['id'];
+$user_id = $_SESSION['id'];
 
-    $post_id = $_POST['post_id'];
+$get_Id = "SELECT * FROM likes_events WHERE User_ID = $user_id AND Event_ID = $post_id;";
 
-    $get_Id = "SELECT * FROM likes_events WHERE User_ID = $user_id AND Event_ID = $post_id;";
+$data = mysqli_query($conn, $get_Id);
 
-    $data = mysqli_query($conn, $get_Id);
-
-    while($row = mysqli_fetch_assoc($data))
-    {
-        $Like_ID = $row['Like_ID'];
-    }
-
-    $SQL = "DELETE FROM likes_events WHERE Like_ID = $Like_ID;";
-
-    $stmt = $conn->prepare($SQL);
-
-    $stmt->execute();
-
-    $conn->close();
-
-    update_likes($post_id);
-
-    header("location: Events.php");
-
-} else {
-
-    header("location: Events.php");
+while($row = mysqli_fetch_assoc($data))
+{
+    $Like_ID = $row['Like_ID'];
 }
+
+$SQL = "DELETE FROM likes_events WHERE Like_ID = $Like_ID;";
+
+$stmt = $conn->prepare($SQL);
+
+$stmt->execute();
+
+$conn->close();
+
+update_likes($post_id);
+
 
 
 function update_likes($post_id)
@@ -48,4 +40,3 @@ function update_likes($post_id)
     $stmt->execute();
 }
 
-?>
