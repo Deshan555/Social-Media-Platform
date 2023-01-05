8770764b759f5db48649e16867d0d1b11fa4ec44
Jayashanka  Deshan <maxwon555@gmail.com>
2023-01-05 22:13:49 +0530

    add security update

--- init.php
+++ init.php
@@ -1,8 +1,8 @@
 <?php
 
+//session_set_cookie_params(time()+1000,'/','localhost',false,true);
 
-session_set_cookie_params(time()+1000,'/','localhost',false,true);
+//session_start();
 
-session_start();
-
+session_start(['cookie_lifetime' => 43200,'cookie_secure' => true,'cookie_httponly' => true]);
 ?>
